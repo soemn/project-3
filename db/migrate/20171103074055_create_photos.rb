61e563746271c4cd788e8b0c29d7812391deb224
sm <a0086845@u.nus.edu>
2017-11-03 15:45:46 +0800

    added photos

---
+++ db/migrate/20171103074055_create_photos.rb
@@ -0,0 +1,10 @@
+class CreatePhotos < ActiveRecord::Migration[5.1]
+  def change
+    create_table :photos do |t|
+      t.string :name
+      t.string :link
+
+      t.timestamps
+    end
+  end
+end
