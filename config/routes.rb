6f8b0d7e5eb5513a5306e4fe1da205f46b67131b
sm <a0086845@u.nus.edu>
2017-11-07 15:20:01 +0800

    created devise views and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'welcome/index'
+  # devise_for :users
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login', sign_out: 'logout',
+    sign_up: 'register', edit: 'profile/edit'
+  }
+
+  # get 'welcome/index'
   root 'welcome#index'
   # login and register routes ------------------------------------
   # get 'users/new'
