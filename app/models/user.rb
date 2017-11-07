fc6ac458ac7f000c061c2ed36c7e84ae67042944
sm <a0086845@u.nus.edu>
2017-11-07 14:41:39 +0800

    updated user login to use devise gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,6 @@
 class User < ApplicationRecord
-  validates :email,
-            presence: true,
-            uniqueness: { case_sensitive: false }
-
-  validates :password_digest,
-            length: { in: 8..72 },
-            on: :create
-
-  has_secure_password
-
-  def self.find_and_authenticate_user(params)
-    User.find_by_email(params[:email]).try(:authenticate, params[:password_digest])
-  end
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'welcome/index'
   root 'welcome#index'
   # login and register routes ------------------------------------
   # get 'users/new'
-  get 'register' => 'users#new'
-  post 'register' => 'users#create'
+  # get 'register' => 'registrations#create'
+  # post 'register' => 'devise/sessions#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
