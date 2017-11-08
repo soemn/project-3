77fa1193c95401d0c415f6f1d20a668dd8a4e6c9
sm <a0086845@u.nus.edu>
2017-11-08 16:14:22 +0800

    updated photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,24 @@
 class PhotosController < ApplicationController
+  def index
+    @photos = Photo.all
+
+    render json: @photos
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+
+    render json: @photo
+  end
+
+  def create
+    # find current_user first
+    # create new project based on that user
+    current_user.photos.create(params.require(:photo).permit(:title))
+
+    redirect_to root_path
+  end
+
   def new
     @new_photo = Photo.new
   end
