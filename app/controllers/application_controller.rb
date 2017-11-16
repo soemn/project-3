521dfab8b3d2c1417e5af08d7928210a8c2efe97
sm <a0086845@u.nus.edu>
2017-11-16 22:55:06 +0800

    cleaned up html.erb code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :slug, :profile_picture])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name slug profile_picture])
   end
 end
